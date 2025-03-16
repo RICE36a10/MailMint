6358d7fe0354ae825619e1c5bcf518992de17370
RICE36a10 <111008452+RICE36a10@users.noreply.github.com>
2025-03-16 12:37:48 +0530

    Hero component import error

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Button } from "../components/ui/button";
 import { Header } from "@/components/custom/Header";
-import { Hero } from "@/components/custom/Hero";
+import Hero from "@/components/custom/Hero";
 export default function Home() {
     return (
         <div>
